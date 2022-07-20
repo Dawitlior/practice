1253a95c4c5441fe301e20be0ee6a3477bb92407
Dawitlior <dawitlior777@gmail.com>
2022-07-20 10:18:43 +0300

    add

--- main.js
+++ main.js
@@ -44,4 +44,48 @@ class Tracks extends MainCar{
     }
 }
 
+class Car{
+    constructor(numberOfWheels,engine,color){
+        this.numberOfWheels=numberOfWheels;
+        this.engine=engine;
+        this.color=color;
+    }
+    getDetails(){
+        return `Number Of Wheels:${this.numberOfWheels} Engine${this.engine} Color:${this.color}`;
+    }
+}
 
+class SmallCar extends Car{
+ constructor(numberOfWheels,engine,color){
+    super(numberOfWheels, engine, color);
+ }
+ getDetails(){
+    return super.getDetails()
+ }   
+}
+
+class newCar extends SmallCar{
+    constructor(numberOfWheels,engine,color){
+        super(numberOfWheels,engine,color)}
+        myFunc(){ 
+            return this.getDetails()
+        }
+}
+let xs = new newCar(4,45,"black");
+console.log(xs);
+let li = new SmallCar(3,122,"lil");
+console.log(li.getDetails());
+
+
+ function azeret(num1){
+      let sum = 1;
+     while(1<=num1){
+         sum*=num1;
+         num1--;
+     }
+     return sum;
+ }
+ console.log(azeret(5));
+
+
+ 
